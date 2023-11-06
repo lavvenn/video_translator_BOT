8e5f3871d568c86bc8527e0fe9acb639d20228c4
lavvenn <zmievsijkonstantin0@gmail.com>
2023-11-06 18:37:35 +0300

    add video handler. downloader for videos

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,8 @@
+import logging
+import os
+
 #подключение aiogram
-from aiogram import  Router, F      
+from aiogram import Bot, Router, F      
 from aiogram.types import Message
 
 #подключение переводчика
@@ -9,6 +12,8 @@ import argostranslate.translate
 
 from_code = "ru"
 to_code = "en"
+
+VIDEO_FOLDER = 'videos/'
 
 # Download and install Argos Translate package
 argostranslate.package.update_package_index()
@@ -52,7 +57,7 @@ BOT is STARTED
 """, reply_markup= kb.user_main)
    
 
-@router.message(F.text == "H2")
+@router.message(F.text == "H1")
 async def vac(message: Message):
         await message.answer(f"""
 TEXT               
@@ -66,6 +71,28 @@ async def help_comand(message: Message):
 TEXT
 """
     )
+
+# Обработчик для приема видео и сохранения их в папку
+@router.message(F.video)
+async def handle_video(message: Message):
+    from config import TOKEN
+    bot = Bot(TOKEN)
+    video = message.video
+    file_id = video.file_id
+    file_info = await bot.get_file(file_id)
+    video_file = await bot.download_file(file_info.file_path)
+    
+    # Создаем папку, если её нет
+    if not os.path.exists(VIDEO_FOLDER):
+        os.makedirs(VIDEO_FOLDER)
+    
+    # Сохраняем видео в папку бота
+    video_path = os.path.join(VIDEO_FOLDER, file_info.file_path)
+    print(video_path)
+    with open(video_path, 'wb') as video_file_local:
+        video_file_local.write(video_file.read())
+    
+    await message.answer(f'Видео сохранено в {video_path}')
 
 #handler который принемает все сообщения и возврощяет ответ от переводчика
 @router.message()                                              
